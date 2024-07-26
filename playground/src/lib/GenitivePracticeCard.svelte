588a238881d4320c76c0ed04a7b3ae91d5920f8d
svidovich <samuel.vidovich@gmail.com>
2024-07-26 00:00:27 -0400

    First swing Sara's genitive with pure svelte

---
+++ playground/src/lib/GenitivePracticeCard.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let sentence;
+  export let answer;
+  export let grammaticalNumber;
+  export let nominative;
+  export let gender;
+  const genitiveSingularEndings = ["a", "e"];
+  const genitivePluralEndings = ["a"];
+
+  let inputValue = "";
+</script>
+
+<div class="container">
+  <p>
+    Use the <em>{grammaticalNumber} genitive</em> for {nominative}, which is a {gender}
+    noun in {grammaticalNumber}
+    nominative.
+  </p>
+  <h3>
+    {sentence}
+  </h3>
+  <input bind:value={inputValue} placeholder="" />
+  {inputValue === answer ? "✅" : "❌"}
+</div>
+
+<style>
+  .container {
+    border: 2px dotted white;
+    padding: 10px;
+  }
+</style>
